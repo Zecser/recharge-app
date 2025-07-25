186200311f1c2fa9f2af2bc0e0b99045f80f874b
farhansherinvm <farhanasherinvm@gmail.com>
2025-07-26 02:15:06 +0530

    Fix:Ensure custom email backend works for login

--- accounts/views.py
+++ accounts/views.py
@@ -445,6 +445,8 @@ def login_email(request):
         
 
         user = authenticate(request, email=email, password=password)
+        if not user:
+            user = authenticate(request, username=email, password=password)
 
         print("Trying to authenticate:", email)
        
