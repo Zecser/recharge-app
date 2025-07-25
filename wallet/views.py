e3bd137b15ee085b6f63918aca7bd5a507da9cf8
farhansherinvm <farhanasherinvm@gmail.com>
2025-07-25 20:32:32 +0530

    fix: Update wallet views

--- wallet/views.py
+++ wallet/views.py
@@ -6,6 +6,7 @@ from django.db import transaction
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+
 from .models import Wallet, WalletTransaction, UserMargin
 from .serializers import (
     WalletSerializer, WalletTransactionSerializer, AddToWalletSerializer,
