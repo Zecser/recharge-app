afb2cd7d2fadb5ea640d72ef3c91eb7b69f8ef4b
farhansherinvm <farhanasherinvm@gmail.com>
2025-07-26 06:23:31 +0530

    Disable browsable API to fix TemplateDoesNotExist error

--- config/settings.py
+++ config/settings.py
@@ -189,6 +189,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+     "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+    ),
 }
 
 # OTP Settings
