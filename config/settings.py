82919ef63319ad954860957baa3e2a2f56105c8e
farhansherinvm <farhanasherinvm@gmail.com>
2025-07-26 07:18:59 +0530

    fix TemplateDoesNotExist error

--- config/settings.py
+++ config/settings.py
@@ -189,9 +189,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-     "DEFAULT_RENDERER_CLASSES": (
-        "rest_framework.renderers.JSONRenderer",
-    ),
+    #  "DEFAULT_RENDERER_CLASSES": (
+    #     "rest_framework.renderers.JSONRenderer",
+    # ),
 }
 
 # OTP Settings
